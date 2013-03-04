735214f5b54cf4e557b553d473f422d264a2b5d0
binnng <imbinnng@gmail.com>
2013-03-04 15:21:31 +0800

    create the repo

---
+++ README.md
@@ -0,0 +1,58 @@
+## 描述
+**bugoo.js** 是一个适用于pc和mobile浏览器的音频播放类库。
+
+### 特性
+* 暂且只支持mp3音频（其他音频也可放，但不保证质量）
+* 支持HTML5的audio接口使用HTML5播放
+* 不依赖任何前端类库
+
+### 浏览器支持
+* ie6+ 及 各种现代浏览器
+* 移动端浏览器
+
+## 文档
+
+### 示例
+
+```javascript
+var sound = new Bugoo({
+    media: 'sound.mp3', //音频文件的地址（必须）
+    swfFile: 'player.swf', //flash文件的地址，不支持audio api的浏览器使用（必须）
+    duration: 60, //音频的时长，以s为单位（必须）
+    loading: function() {
+      //加载时执行的方法
+    },
+    start: function() {
+      //开始播放时执行的方法
+    },
+    timeupdate: function() {
+      //播放过程中执行的方法
+    },
+    stop: function() {
+      //停止时执行的方法
+    }
+});
+```
+
+##### 立即播放:
+```javascript
+var sound = new Bugoo({
+    media: 'sound.mp3',
+    swfFile: 'player.swf',
+    duration: 60
+}).play();
+```
+
+##### 播放
+```javascript
+sound.play();
+```
+##### 停止
+```javascript
+sound.stop();
+```
+
+### 属性
+* **duration**: `Number` 音频的时长（以s为单位）
+* **currentTime**: `Number` 当前播放的进度（以s为单位）
+* **status**: `String` 播放的状态。有`ready`，`loading`，`playing`和`stoped`四个返回值。
---
+++ js/bugoo.js
@@ -0,0 +1,197 @@
+/*
+ *音频插件，支持pc和手持设备，一个页面只允许一个音频
+ *@time 2013/3/2
+ *@demo 
+ *var audio = new Bugoo({
+ *  media: 'a.mp3',
+ *  swfPath: 'a/swf/player.swf',
+ *  duration: 280,
+ *  start: function() {},
+ *  loading: function() {},
+ *  end: function() {}
+ *});
+ */
+
+var Bugoo = function(window, undefined) {
+
+	//是否支持Flash
+	function hasFlash() {
+		var bool = false;
+		if(navigator.plugins && navigator.plugins.length){
+			var l = navigator.plugins.length;
+		  	while(l--){
+		   		if(navigator.plugins[l].name.indexOf('Shockwave Flash')!=-1){
+		    		bool=true;
+		    		break;
+		    	}
+		 	}
+		}else if(window.ActiveXObject){
+		  for (var i=10;i>=2;i--){
+		    try{   
+		      var t=eval("new ActiveXObject('ShockwaveFlash.ShockwaveFlash." + i + "');");
+		      if(t){
+		      	bool=true;
+		      	break;
+		      }
+		    }
+		    catch(e) {}
+		  }
+		}
+		return bool;
+	}
+
+	//flash Object对象的方法，注册到window
+	window.getTime = function () {
+		var flashObj = window['mkk'] || document['mkk'],
+		time = 0;
+
+		if (flashObj && (flashObj.getTime || (flashObj = flashObj[1]) && flashObj.getTime)) {
+			time = flashObj.getTime();
+			if (isNaN(time)) {
+				time = 0;
+			}
+		}
+		
+		return time;
+	}
+
+	var body = document.body || document.getElementsByTagName('html')[0],
+
+		audio = document.createElement('audio'),
+
+		//是否可以播放mp3
+		canPlayMp3 = !! ( audio.canPlayType && audio.canPlayType('audio/mp3') ),
+
+		//stop
+		stop,
+
+		//是否用flash播放，待改进
+		/*playByFlash = !('ontouchstart' in window) 
+					&& document.documentMode != 10 
+					&& document.documentMode != 9 
+					&& hasFlash(),*/
+
+		//放置flash代码的元素
+		bugooFlashElement,
+		flashHTML,
+		timer,
+
+		//空函数，省去判断函数有没有再去执行的判断
+		emptyFn = function(){};
+
+	if ( !canPlayMp3 ) {
+
+		audio = undefined;
+
+		bugooFlashElement = document.createElement('div');
+		flashHTML = '<object id="mkk" name="mkk" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="1"  height="0" ><param name="allowScriptAccess" value="always" /><param name="movie" value="{swfFile}?audioUrl={audioUrl}" /><param name="quality" value="high" /><embed id="mkk" name="mkk" src="{swfFile}?audioUrl={audioUrl}" allowScriptAccess="sameDomain" allowFullScreen="true"  FlashVars=""  quality="high"  width="1"  height="1"  align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></object>';				
+		bugooFlashElement.setAttribute('style', "height:0;overflow:hidden");
+
+		body.appendChild(bugooFlashElement);
+
+	} else {
+		delete bugooFlashElement;
+		delete flashHTML;
+	}
+
+	var bugoo = function(conf) {
+
+		this.media        = conf.media;
+		this.swfFile      = conf.swfFile;
+		this.currentTime  = 0;
+		this.duration     = conf.duration;
+		this.status       = 'ready';
+		this.startFn      = conf.start      || emptyFn;
+		this.loadingFn    = conf.loading    || emptyFn;
+		this.timeupdateFn = conf.timeupdate || emptyFn;
+		this.stopFn       = conf.stop       || emptyFn;
+
+		//一个页面只有一个实例
+		if ( 'bugooUniqueInstance' in window ) {
+			window.bugooUniqueInstance.stop();
+		} else {
+			window.bugooUniqueInstance = this;
+		}
+
+	};
+
+	bugoo.prototype.play = function() {
+		var that = this;
+
+		if (audio) {
+			audio.src = that.media;
+			audio.play();
+
+			audio.addEventListener('play', that.startFn);
+			audio.addEventListener('playing', function() {
+				that.status = 'playing';
+			});
+			audio.addEventListener('waiting', function() {
+				that.status = 'loading';
+				that.loadingFn();
+			});
+			/*
+			 *不能这么写
+			 *audio.addEventListener('ended',that.stop);
+			 *这样会改变stop里的this指向
+			 */
+			audio.addEventListener('ended', function() {
+				that.stop();
+			});
+
+			timer = setInterval(function() {
+
+				that.currentTime = audio.currentTime;
+
+				that.timeupdateFn();
+
+			}, 30);
+		} else {
+
+			bugooFlashElement.innerHTML = flashHTML.replace(/{swfFile}/g, that.swfFile).replace(/{audioUrl}/g, that.media);
+			timer = setInterval(function() {
+
+				if ( 0 === getTime() || NaN === getTime() ) {
+					that.status = 'loading';
+					that.loadingFn();
+				} else {
+					that.startFn && that.startFn();
+					//startFn 只触发一次
+					delete that.startFn;
+					that.status = 'playing';
+					that.timeupdateFn();
+				}					
+
+				if(getTime() === 100) {
+					that.stop();
+				}
+
+				that.currentTime = getTime() * that.duration / 100;
+
+			}, 30);
+		}
+
+		return that;
+	};
+
+	bugoo.prototype.stop = function() {
+		var that = this;
+
+		if (audio) {
+			audio.src = audio.currentSrc;
+		} else {
+			bugooFlashElement.innerHTML = '';
+		}
+
+		clearInterval(timer);
+
+		that.status = 'stoped';
+
+		that.stopFn();
+
+		return that;
+	};
+
+	return bugoo;
+
+}(window);
